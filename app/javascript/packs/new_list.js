e2048b4264d34a0647d37b4d578ea500e3b2c618
Luis Novoa <luis.matte.novoa@gmail.com>
2020-11-03 00:19:58 -0400

    Improves the addition and deletion of tasks on the new list form

--- app/javascript/packs/new_list.js
+++ app/javascript/packs/new_list.js
@@ -1,27 +1,56 @@
 $(document).ready(function () {
-  // const test = "<h1>I'm the god of Jquery :3</h1>"
-  // $('.new-list-form').prepend(test);
+  $('.tasks').find('ul').data({
+    taskCounter: 1,
+    sublistCounter: 0,
+    parentID: 'list',
+    parentName: 'list'
+  });
 
-  $('.delete-task').click(removeParent);
+  $('.delete-task').click(removeParentTask);
 
   $('.add-task').click(function (e) {
     e.preventDefault();
-    if ($('li.task').last().html() == undefined) {
-      const newTaskLabel = "<label for='list_tasks_attributes_0_description'>Description</label>";
-      const newTaskInput = $('<input></input>').attr({
-        'type': 'text',
-        'name': 'list[tasks_attributes][0][description]',
-        'id': 'list_tasks_attributes_0_description'
-      });
-      const deleteButton = $("<button class='delete-task'>Delete this Task</button>").click(removeParent);
-      const newTask = $("<li class='task'></li>").append(newTaskLabel, newTaskInput, deleteButton);
-      $(this).parent().prepend(newTask);
-      return;
-    }
+    // const newTaskLabel = "<label for='list_tasks_attributes_0_description'>Description</label>";
+    // const newTaskInput = $('<input></input>').attr({
+    //   'type': 'text',
+    //   'name': 'list[tasks_attributes][0][description]',
+    //   'id': 'list_tasks_attributes_0_description'
+    // });
+    // const deleteButton = $("<button class='delete-task'>Delete this Task</button>").click(removeParentTask);
+    const { input, label, deleteButton } = createElements('task', $(this).parent());
+    const newTask = $("<li class='task'></li>").append(label, input, deleteButton);
+    $(this).before(newTask);
+    updateCounter('task', 1, $(this).parent());
   });
 });
 
-function removeParent(e) {
+function createElements(type, object) {
+  const attr = type + 's_attributes';
+  const counter = object.data(`${type}Counter`);
+  const ancestorsID = object.data('parentID');
+  const ancestorsName = object.data('parentName');
+  const id = `${ancestorsID}_${attr}_${counter}_description`;
+  const name = `${ancestorsName}[${attr}][${counter}][description]`
+  const deleteButton = $("<button class='delete-task'>Delete this Task</button>").click(removeParentTask);
+  const label = `<label for='${id}'>Description</label>`;
+  const input = $('<input></input>').attr({
+    'type': 'text',
+    'name': name,
+    'id': id
+  });
+  return {
+    label: label,
+    input: input,
+    deleteButton: deleteButton
+  };
+}
+
+function removeParentTask(e) {
   e.preventDefault();
+  updateCounter('task', -1, $(this).parent().parent());
   $(this).parent().remove();
+}
+
+function updateCounter(type, amount, object) {
+  object.data(`${type}Counter`, object.data(`${type}Counter`) + amount);
 }
